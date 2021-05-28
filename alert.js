7cebecd4a7d28c4ee5a300805697217785d63b64
AhmedRaza1211 <50639350+AhmedRaza1211@users.noreply.github.com>
2021-05-28 07:10:35 +0500

    update code

--- alert.js
+++ alert.js
@@ -508,8 +508,21 @@
 // }
 
 
-// 3. Write a program to take input color of road traffic signal 
-// from the user & show the message according to this table:
+// // 3. Write a program to take input color of road traffic signal 
+// // from the user & show the message according to this table:
+// var color = prompt("Enter any color of traffic light");
+// if (color === "Red" || color === "red") {
+//     document.write("Must Stop");
+// } else if (color === "Yellow" || color === "yellow") {
+//     document.write("Ready to move");
+// } else if (color === "Green" || color === "green") {
+//     document.write("Move now");
+// }
+
+
+
+
+
 
 
 
